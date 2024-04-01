e885d18e432ea45bd79a460ada26b282e0067503
Saksham2k3s <sakshamshrivastav58@gmail.com>
2024-04-01 19:35:18 +0530

    fixed issue in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -8,11 +8,8 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
-
-
-
